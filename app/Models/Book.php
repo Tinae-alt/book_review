a9ab323a10446a1716a839f2785f3f5ccf7b8757
Langton Chitiga <langtonchitiga@gmail.com>
2024-12-24 04:13:04 +0200

    added relationship between book and reviews tables

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     //
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     //
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
